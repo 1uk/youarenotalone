5e41811c3a17db701a014e7e85cfe426636a6d8c
Lukas Kitsche <lukaskitsche@posteo.de>
2020-05-05 11:14:04 +0200

    make circle follow mouse

--- bundle.js
+++ bundle.js
@@ -22415,15 +22415,12 @@ var paper = require('paper');
 		// Create an empty project and a view for the canvas:
 		paper.setup(canvas);
 		// Create a Paper.js Path to draw a line into it:
-		var path = new paper.Path();
+		var path = new paper.Path.Circle(new paper.Point(100, 100), 20);
 		// Give the stroke a color
 		path.strokeColor = 'black';
-		var start = new paper.Point(100, 100);
-		// Move to start and draw a line from there
-		path.moveTo(start);
-		// Note that the plus operator on Point objects does not work
-		// in JavaScript. Instead, we need to call the add() function:
-		path.lineTo(start.add([ 200, -50 ]));
+		paper.view.onMouseMove = function(event) {
+		path.position = event.point;
+		};
 		// Draw the view now:
 		paper.view.draw();
 	}
