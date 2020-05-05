8d1b0b91e5f6dbf0780fcf10139ffd1a476844a6
Lukas Kitsche <lukaskitsche@posteo.de>
2020-05-05 11:06:17 +0200

    add browserify and expressjs

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+var paper = require('paper');
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
+});
+app.get('/bundle.js', (req, res) => {
+	res.sendFile(__dirname + '/bundle.js');
 });
 
 io.on('connection', (socket) => {
